48ac7c6da96c6bafb83295a7ba29507afbc26f1b
jaksa97 <jaksa97@gmail.com>
2024-08-16 13:04:34 +0200

    Initial commit

---
+++ src/main/kotlin/com/github/jaksa97/LeafSaver_Kotlin/LeafSaverKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.github.jaksa97.LeafSaver_Kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeafSaverKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<LeafSaverKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/jaksa97/LeafSaver_Kotlin/LeafSaverKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.jaksa97.LeafSaver_Kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeafSaverKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
